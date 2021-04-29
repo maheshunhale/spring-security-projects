25d9bf71489a74d6a642b13431648d359aaa009f
maheshunhale <42297257+maheshunhale@users.noreply.github.com>
2021-04-29 18:41:29 +0530

    updated

--- firstApp - Spring Security Basic Authentication/src/main/java/com/mahesh/spring/security/MySecurityConfig.java
+++ firstApp - Spring Security Basic Authentication/src/main/java/com/mahesh/spring/security/MySecurityConfig.java
@@ -3,7 +3,6 @@ package com.mahesh.spring.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -18,9 +17,6 @@ public class MySecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	@Autowired
 	private PasswordEncoder passwordEncoder;
-	
-	@Autowired
-	private AuthenticationProvider authenticationProvider;
 	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
