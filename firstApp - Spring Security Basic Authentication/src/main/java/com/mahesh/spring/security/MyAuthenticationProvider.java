80028d59eefb203fb9d637f55b2ac214179f6828
maheshunhale <42297257+maheshunhale@users.noreply.github.com>
2021-04-29 18:46:48 +0530

    updated

---
+++ firstApp - Spring Security Basic Authentication/src/main/java/com/mahesh/spring/security/MyAuthenticationProvider.java
@@ -0,0 +1,32 @@
+package com.mahesh.spring.security;
+
+import java.util.Arrays;
+
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyAuthenticationProvider implements AuthenticationProvider{
+
+	@Override
+	public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+		String userName = authentication.getName();
+		String password = authentication.getCredentials().toString();
+
+		if ("mahesh".equals(userName) && "mahesh".equals(password)) {
+			return new UsernamePasswordAuthenticationToken(userName, password, Arrays.asList());
+		} else {
+			throw new BadCredentialsException("Invalid UserName or Password.");
+		}
+	}
+
+	@Override
+	public boolean supports(Class<?> authentication) {
+		return authentication.equals(UsernamePasswordAuthenticationToken.class);
+	}
+
+}
